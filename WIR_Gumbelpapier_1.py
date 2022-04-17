4737df59cbecdac6e9e55109a52bce9838a5d494
WIR4713 <wfhsoft@gmail.com>
2022-04-17 07:27:20 +0200

    README

--- WIR_Gumbelpapier_1.py
+++ WIR_Gumbelpapier_1.py
@@ -3,8 +3,21 @@
 WIR_Gumbel_1
 ==========
 Script zum Erzeugen eines Wahrscheinlichkeitspapiers für die Gumbelverteilung
-Stand: 16.04.2022
-Kontakt: Dr.-Ing. Wilhelm Riebe (wfhsoft@gmail.com)
+
+Copyright (c) 2022 Dr.-Ing. Wilhelm Riebe (wfhsoft@gmail.com)
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import numpy as np
 import matplotlib.pyplot as plt
